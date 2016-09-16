efc97c0470b99bb3e639a04642283e96f7b4ed7f
Gabriel Sjöberg <ga22be@live.se>
2016-09-16 11:30:22 +0200

    Fixd SQL-script

--- sql/init.sql
+++ sql/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE Users(
 	_id INTEGER AUTO_INCREMENT NOT NULL,
 	username VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL, -- Suitable length for BCRYPT / BLOWFISH
-	salt VARCHAR(32) NOT NULL, -- ? Decide on a salt size
+	-- salt VARCHAR(32) NOT NULL, -- ? Decide on a salt size
 	PRIMARY KEY(_id)
 );
 
@@ -14,10 +14,11 @@ CREATE TABLE Items(
 	_id INTEGER NOT NULL AUTO_INCREMENT,
 	name VARCHAR(32) NOT NULL UNIQUE,
 	description VARCHAR(255),
+	price DOUBLE, 
 	PRIMARY KEY(_id)
 );
 
-INSERT INTO Users(username, password, salt) VALUES('test1', 'test1', 'test1');
-INSERT INTO Users(username, password, salt) VALUES('test2', 'test2', 'test2');
-INSERT INTO Items(name, description) VALUES('test1', 'test1');
-INSERT INTO Items(name, description) VALUES('test2', 'test2');
+INSERT INTO Users(username, password) VALUES('test1', 'test1');
+INSERT INTO Users(username, password) VALUES('test2', 'test2');
+INSERT INTO Items(name, description, price) VALUES('test1', 'test1', 9.99);
+INSERT INTO Items(name, description, price) VALUES('test2', 'test2', 19.99);
