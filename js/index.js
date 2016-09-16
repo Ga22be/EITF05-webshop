3baca5b64dcc21475a0c6aba38e9782f4cb00ef1
Emil <e.initium@gmail.com>
2016-09-16 12:29:39 +0200

    login buttons impl."

--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
-console.log('Hello world!')
+$('#btn-login').click(() => {
+	console.log('Login clicked');
+
+	$.ajax({
+		type: 'POST',
+		url: '../php/login.php',
+		data: $('#form-logreg').serialize(),
+		dataType: 'json',
+		success: (data) => {
+			if (data.error == true) {
+				console.log('invalid credentials');
+			} else {
+				console.log('valid credentials');
+			}
+		},
+		beforeSend: () => {
+			$('#btn-login').html('Loading...');
+		},
+		complete: () => {
+			console.log('ajax completed');
+			$('#btn-login').html('Login');
+		},
+		error: (e) => {
+			console.error(e);
+		}
+	});
+
+	return false;
+});
+
+$('#btn-register').click(() => {
+	console.log('Register clicked');
+	$.ajax({
+		type: 'POST',
+		url: '../php/register.php',
+		data: $('#form-logreg').serialize(),
+		dataType: 'json',
+		success: (data) => {
+			if (data.error == true) {
+				console.log('invalid credentials');
+			} else {
+				console.log('valid credentials');
+			}
+		},
+		beforeSend: () => {
+			$('#btn-register').html('Loading...');
+		},
+		complete: () => {
+			console.log('ajax completed');
+			$('#btn-register').html('Login');
+		},
+		error: (e) => {
+			console.error(e);
+		}
+	});
+
+	return false;
+});
