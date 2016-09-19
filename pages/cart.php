48e1f4cef085e65cced1e9192b2131ca165cb339
Emil <e.initium@gmail.com>
2016-09-19 13:38:33 +0200

    cart boiler

---
+++ pages/cart.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('../php/database.php');
+
+//if (isset($_SESSION['cart'])) {
+	$db = new Database();
+	$items = $db->getItems();
+
+	/*
+	foreach($items as $key) {
+		print $key['_id'];
+		print $key['name'];
+		print '<br>';
+	}
+	 */
+
+	$cart = [];
+	$itemNames = [];
+
+	foreach($items as $key) {
+		$cart[$key['_id']] = 3;
+		$itemNames[$key['_id']] = $key['name'];
+	}
+
+	$_SESSION['cart'] = $cart;
+
+	foreach ($_SESSION['cart'] as $key => $value) {
+		print $itemNames[$key] . ' ' . $value . '<br>';
+	}
+//} else {
+	//echo '<b>Cart is empty.</b>';
+//}
+?>
+
+
