c3719ae1e07930f01ec193f564706392b5bbf685
Gabriel Sjöberg <ga22be@live.se>
2016-09-19 15:06:24 +0200

    Pushing to continue on other location

--- pages/cart.php
+++ pages/cart.php
@@ -25,12 +25,39 @@ require_once('../php/database.php');
 
 	//$_SESSION['cart'] = $cart;
 
+/*
 	foreach ($_SESSION['cart'] as $key => $value) {
 		print $itemNames[$key] . ' ' . $value . '<br>';
 	}
+*/
 //} else {
 	//echo '<b>Cart is empty.</b>';
 //}
 ?>
 
-
+<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8">
+	<title>Sidenvägen</title>
+	<link href="/favicon.ico" rel="icon" type="image/x-icon" />
+	<style type="text/css">
+		@import 'https://fonts.googleapis.com/css?family=Roboto';
+		@import '../css/cart.css';
+		@import '../css/global.css';
+	</style>
+</head>
+<body>
+	<div id="header">
+    <h1>Here Goes The Title</h1>
+  </div>
+	<div id="container">
+    <p>This is your current cart <?php echo $_SESSION['username'] ?></p>
+	</div>
+	<div id="cartContainer">
+		<?php
+			foreach ($_SESSION['cart'] as $key => $value) {
+				echo "<div id=cartItem>";
+				echo "<p id=iHeader>" . $itemNames[$key] . "</p>";
+			}
+		?>
+	</div>
+</body>
+</html>
