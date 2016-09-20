ffb05962dff095574dc96209f1ed2d6a15c245b8
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-09-20 18:19:10 +0200

    compliant to more than puccini

--- sql/init_items.sql
+++ sql/init_items.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Items(
+CREATE TABLE items(
 	_id INTEGER NOT NULL AUTO_INCREMENT,
 	name VARCHAR(32) NOT NULL UNIQUE,
 	description text,
@@ -7,7 +7,7 @@ CREATE TABLE Items(
 	PRIMARY KEY(_id)
 );
 
-INSERT INTO Items(name, description, price, image) VALUES('Deadpool', 'A higly acclaimed blockbuster from 2016', 9.99, "../pictures/deadpool.jpg");
-INSERT INTO Items(name, description, price, image) VALUES('Chromecast', 'Google Chromecast 2nd generation', 19.99, "../pictures/chromecast2.jpg");
-INSERT INTO Items(name, description, price, image) VALUES('iPhone7', 'Apple\'s latest smartphone', 649, "../pictures/iphone7.jpg");
-INSERT INTO Items(name, description, price, image) VALUES('FIFA17', 'Be the first to experience EA-games latest money-grab', 60, "../pictures/fifa17.jpg");
+INSERT INTO items(name, description, price, image) VALUES('Deadpool', 'A higly acclaimed blockbuster from 2016', 9.99, "../pictures/deadpool.jpg");
+INSERT INTO items(name, description, price, image) VALUES('Chromecast', 'Google Chromecast 2nd generation', 19.99, "../pictures/chromecast2.jpg");
+INSERT INTO items(name, description, price, image) VALUES('iPhone7', 'Apple\'s latest smartphone', 649, "../pictures/iphone7.jpg");
+INSERT INTO items(name, description, price, image) VALUES('FIFA17', 'Be the first to experience EA-games latest money-grab', 60, "../pictures/fifa17.jpg");
--- sql/init_users.sql
+++ sql/init_users.sql
@@ -1,13 +1,14 @@
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS users;
 SET foreign_key_checks = 1;
 
-CREATE TABLE Users(
+CREATE TABLE users(
 	_id INTEGER AUTO_INCREMENT NOT NULL,
 	username VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL, -- Suitable length for BCRYPT / BLOWFISH
 	address VARCHAR(255) NOT NULL,
 	fails INTEGER NOT NULL DEFAULT 0,
-	locked TIMESTAMP NOT NULL DEFAULT 0,
+	locked TIMESTAMP NOT NULL,
 	PRIMARY KEY(_id)
 );
