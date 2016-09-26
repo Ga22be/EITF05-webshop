33d3ceb8927f9000dd16f726bce6f91a30fdafe2
Gabriel Sjöberg <ga22be@live.se>
2016-09-26 12:26:54 +0200

    Refined cart page

--- pages/cart.php
+++ pages/cart.php
@@ -20,10 +20,9 @@
 
 	//$cart = [];
 	$itemNames = [];
-
 	foreach($items as $key) {
 		//$cart[$key['_id']] = 3;
-		$itemNames[$key['_id']] = $key['name'];
+		$itemNames[$key['_id']] = [$key['name'], $key['price']];
 	}
 
 	//$_SESSION['cart'] = $cart;
@@ -49,19 +48,41 @@
 	</style>
 </head>
 <body>
-	<div id="header">
-    <h1>Here Goes The Title</h1>
-  </div>
-	<div id="container">
-    <p>This is your current cart <?php echo $_SESSION['username'] ?></p>
+	<div id="topBar">
+		<div id="header">
+			<a href='home.php'>Sidenvägen</a>
+		</div>
+		<div id="user">
+			<?php echo '<span id="userName">' . $_SESSION['username'] . '</span>' ?>
+		</div>
+		<div id="cartContent">
+			<img id="cartImg" src="../pictures/cart.png"></img>
+			<div id="cartAmount"><#></div>
+		</div>
+		<div id='topButtons'>
+			<button id="btn-logout" class="btn-top waves-effect" onclick="location.href = '../php/logout.php';">Logout</button>
+		</div>
 	</div>
 	<div id="cartContainer">
+		<table <?php if(count($_SESSION['cart']) == 0) {echo "class=\"hide\"";} ?>>
+			<tr id='tHeader'>
+				<th id='product'>Product</th><th>Amount</th><th>Price/Item</th><th>Price</th>
+			</tr>
 		<?php
+			$total = 0;
 			foreach ($_SESSION['cart'] as $key => $value) {
-				echo "<div id=cartItem>";
-				echo "<p id=iHeader>" . $itemNames[$key] . "</p>";
+				echo "<tr>";
+				echo "<td id='product'>" . $itemNames[$key][0] . "	</td>";
+				echo "<td id='amount'>" . $value . "</td>";
+				echo "<td id='price/item'>" . '$' . $itemNames[$key][1] . "</td>";
+				echo "<td id='price'>" . '$' . $itemNames[$key][1]*$value . "</td>";
+				echo "</tr>";
+
+				$total += $itemNames[$key][1]*$value;
 			}
+			echo "<tr><td id='total'>Total:</td><td></td><td></td><td>" . '$' . $total . "</td></tr>";
 		?>
+		</table>
 	</div>
 	<div id="purchaseContainer">
 		<div id="resp"></div>
@@ -72,7 +93,7 @@
 		<form method="post" id="form-login">
 			<input type="text" placeholder="Username" name="username"></input>
 			<input type="password" placeholder="Password" name="password"></input>
-			
+
 			<button id="btn-login">Purchase</button>
 		</form>
 	</div>
