f5c54bd695e23fa17c5bbbfc180867c6b39442a4
Gabriel Sjöberg <ga22be@live.se>
2016-09-26 13:10:53 +0200

    Tinkered with cart

--- pages/cart.php
+++ pages/cart.php
@@ -80,7 +80,7 @@
 
 				$total += $itemNames[$key][1]*$value;
 			}
-			echo "<tr><td id='total'>Total:</td><td></td><td></td><td>" . '$' . $total . "</td></tr>";
+			echo "<tr><td></td><td></td><td></td><td id='sum'>" . '$' . $total . "</td></tr>";
 		?>
 		</table>
 	</div>
